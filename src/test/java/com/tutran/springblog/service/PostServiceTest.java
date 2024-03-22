f0d296509e6f33b32832679345578deef83e1bf3
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-03-22 22:54:51 +0700

    Update post service test to not mock mapper interface

--- src/test/java/com/tutran/springblog/service/PostServiceTest.java
+++ src/test/java/com/tutran/springblog/service/PostServiceTest.java
@@ -2,18 +2,16 @@ package com.tutran.springblog.service;
 
 import com.tutran.springblog.entity.Post;
 import com.tutran.springblog.mapper.PostMapper;
-import com.tutran.springblog.payload.comment.CommentResponseDto;
+import com.tutran.springblog.mapper.PostMapperImpl;
 import com.tutran.springblog.payload.meta.ResponseDtoWithMeta;
-import com.tutran.springblog.payload.post.PostDetailsResponseDto;
 import com.tutran.springblog.payload.post.PostPartialUpdateRequestDto;
-import com.tutran.springblog.payload.post.PostRequestDto;
 import com.tutran.springblog.payload.post.PostResponseDto;
 import com.tutran.springblog.repository.PostRepository;
 import com.tutran.springblog.service.impl.PostServiceImpl;
 import com.tutran.springblog.utils.RandomGenerator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.dao.DuplicateKeyException;
@@ -21,45 +19,38 @@ import org.springframework.data.domain.*;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PostServiceTest {
-    @InjectMocks
-    private PostServiceImpl postService;
-
     @Mock
     private PostRepository postRepository;
 
-    @Mock
-    private PostMapper postMapper;
+    private final PostMapper postMapper = new PostMapperImpl();
+
+    private PostService postService;
+
+    @BeforeEach
+    void beforeEach() {
+        postService = new PostServiceImpl(postRepository, postMapper);
+    }
 
     @Test
     void testCreatePostSuccessfully() {
-        PostRequestDto postRequestDto = RandomGenerator.generateRandomPostRequestDto();
+        var request = RandomGenerator.generateRandomPostRequestDto();
+        var post = postMapper.postRequestDtoToPost(request);
 
-        var post = this.getPostFromPostRequestDto(postRequestDto);
-        var newPost = this.getPostFromPostRequestDto(postRequestDto, RandomGenerator.generateRandomId());
+        when(postRepository.save(any(Post.class))).thenReturn(post);
 
-        when(postMapper.postRequestDtoToPost(postRequestDto)).thenReturn(post);
-        when(postRepository.save(post)).thenReturn(newPost);
-        when(postMapper.postToPostResponseDto(newPost)).thenReturn(this.getPostResponseDtoFromPost(newPost));
-
-        var result = postService.createPost(postRequestDto);
-
-        verify(postMapper).postRequestDtoToPost(postRequestDto);
-        verify(postRepository).save(post);
-        verify(postMapper).postToPostResponseDto(newPost);
+        var result = postService.createPost(request);
 
         assertNotNull(result);
-        assertEquals(newPost.getId(), result.getId());
-        assertEquals(newPost.getTitle(), result.getTitle());
-        assertEquals(newPost.getDescription(), result.getDescription());
-        assertEquals(newPost.getContent(), result.getContent());
+        assertEquals(request.getTitle(), result.getTitle());
+        assertEquals(request.getDescription(), result.getDescription());
+        assertEquals(request.getContent(), result.getContent());
     }
 
     @Test
@@ -78,25 +69,18 @@ class PostServiceTest {
         String sortBy = "title";
         String sortDir = "ASC";
 
-        List<Post> mockPosts = List.of(RandomGenerator.generateRandomPost(), RandomGenerator.generateRandomPost());
+        List<Post> posts = List.of(RandomGenerator.generateRandomPost(), RandomGenerator.generateRandomPost());
 
         Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy).ascending());
-        Page<Post> mockPage = new PageImpl<>(mockPosts, pageable, mockPosts.size());
+        Page<Post> mockPage = new PageImpl<>(posts, pageable, posts.size());
 
         when(postRepository.findAll(pageable)).thenReturn(mockPage);
-        when(postMapper.postToPostResponseDto(any(Post.class))).thenAnswer(invocation -> {
-            Post post = invocation.getArgument(0);
-            return this.getPostResponseDtoFromPost(post);
-        });
 
         ResponseDtoWithMeta<List<PostResponseDto>> result = postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
 
-        verify(postRepository).findAll(pageable);
-        verify(postMapper, times(mockPosts.size())).postToPostResponseDto(any());
-
         assertNotNull(result);
-        assertEquals(mockPosts.size(), result.getData().size());
-        assertEquals(mockPosts.size(), result.getMeta().getTotalElements());
+        assertEquals(posts.size(), result.getData().size());
+        assertEquals(posts.size(), result.getMeta().getTotalElements());
         assertTrue(result.getMeta().isLast());
     }
 
@@ -104,29 +88,9 @@ class PostServiceTest {
     void testGetPostByIdSuccessfully() {
         var post = RandomGenerator.generateRandomPostWithComments();
 
-        var comments = post.getComments();
-        var commentResponses = comments.stream().map(comment -> CommentResponseDto.builder()
-                .id(comment.getId())
-                .name(comment.getName())
-                .email(comment.getEmail())
-                .body(comment.getBody())
-                .build()).collect(Collectors.toSet());
-
         when(postRepository.findById(post.getId())).thenReturn(Optional.of(post));
-        when(postMapper.postToPostDetailsResponseDto(post)).thenReturn(
-                PostDetailsResponseDto.builder()
-                        .id(post.getId())
-                        .title(post.getTitle())
-                        .description(post.getDescription())
-                        .content(post.getContent())
-                        .comments(commentResponses)
-                        .build()
-        );
 
         var result = postService.getPostById(post.getId());
-
-        verify(postRepository).findById(post.getId());
-        verify(postMapper).postToPostDetailsResponseDto(post);
 
         assertNotNull(result);
         assertEquals(result.getId(), post.getId());
@@ -141,17 +105,13 @@ class PostServiceTest {
         var post = RandomGenerator.generateRandomPost();
         post.setId(RandomGenerator.generateRandomId());
 
-        var updateRequest = PostPartialUpdateRequestDto.builder().title(RandomGenerator.generateRandomString()).build();
-        var updatedPost = post.toBuilder().title(updateRequest.getTitle()).build();
+        var request = PostPartialUpdateRequestDto.builder().title(RandomGenerator.generateRandomString()).build();
+        var updatedPost = post.toBuilder().title(request.getTitle()).build();
 
         when(postRepository.findById(post.getId())).thenReturn(Optional.of(post));
         when(postRepository.save(any(Post.class))).thenReturn(updatedPost);
-        when(postMapper.postToPostResponseDto(any(Post.class))).thenReturn(this.getPostResponseDtoFromPost(updatedPost));
 
-        var result = postService.patchUpdatePostById(post.getId(), updateRequest);
-
-        verify(postRepository).findById(post.getId());
-        verify(postMapper).postToPostResponseDto(updatedPost);
+        var result = postService.patchUpdatePostById(post.getId(), request);
 
         assertNotNull(result);
         assertEquals(result.getId(), updatedPost.getId());
@@ -178,17 +138,13 @@ class PostServiceTest {
         var post = RandomGenerator.generateRandomPost();
         post.setId(RandomGenerator.generateRandomId());
 
-        var updateRequest = RandomGenerator.generateRandomPostRequestDto();
-        var updatedPost = this.getPostFromPostRequestDto(updateRequest, post.getId());
+        var request = RandomGenerator.generateRandomPostRequestDto();
+        var updatedPost = postMapper.postRequestDtoToPost(request);
 
         when(postRepository.findById(post.getId())).thenReturn(Optional.of(post));
         when(postRepository.save(any(Post.class))).thenReturn(updatedPost);
-        when(postMapper.postToPostResponseDto(any(Post.class))).thenReturn(this.getPostResponseDtoFromPost(updatedPost));
 
-        var result = postService.updatePostById(post.getId(), updateRequest);
-
-        verify(postRepository).findById(post.getId());
-        verify(postMapper).postToPostResponseDto(updatedPost);
+        var result = postService.updatePostById(post.getId(), request);
 
         assertNotNull(result);
         assertEquals(result.getId(), updatedPost.getId());
@@ -207,26 +163,5 @@ class PostServiceTest {
         var result = postService.deletePostById(post.getId());
 
         assertNotNull(result);
-    }
-
-    private PostResponseDto getPostResponseDtoFromPost(Post post) {
-        return PostResponseDto.builder()
-                .id(post.getId())
-                .title(post.getTitle())
-                .description(post.getDescription())
-                .content(post.getContent())
-                .build();
-    }
-
-    private Post getPostFromPostRequestDto(PostRequestDto postRequestDto) {
-        return Post.builder()
-                .title(postRequestDto.getTitle())
-                .description(postRequestDto.getDescription())
-                .content(postRequestDto.getContent())
-                .build();
-    }
-
-    private Post getPostFromPostRequestDto(PostRequestDto postRequestDto, long postId) {
-        return getPostFromPostRequestDto(postRequestDto).toBuilder().id(postId).build();
     }
 }
